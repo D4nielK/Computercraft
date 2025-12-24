53449bf025d1fec2c4b9556cbc447045195a020b
B3nniB <benjaminbank99@gmail.com>
2025-12-24 04:12:27 +0100

    Testen was geht

--- CC_Reaktor/reaktor.lua
+++ CC_Reaktor/reaktor.lua
@@ -1,2 +1,8 @@
-print("Nein")
-print("ja")
+local r = peripheral.find("fissionReactorLogicAdapter")
+if not r then error("Kein fissionReactorLogicAdapter gefunden") end
+
+local name = peripheral.getName(r)
+print("Peripheral name: "..name)
+local methods = peripheral.getMethods(name)
+table.sort(methods)
+for _,m in ipairs(methods) do print(m) end
