17626bd94d9e72ec111fbf63e24aa394aab7ed87
B3nniB <benjaminbank99@gmail.com>
2025-12-25 01:48:00 +0100

    Peripheral Turbine Test 2

--- CC_Reaktor/reaktor.lua
+++ CC_Reaktor/reaktor.lua
@@ -1,4 +1,5 @@
 ---@diagnostic disable: undefined-global
-for _, n in ipairs(peripheral.getNames()) do
-  print(n, peripheral.getType(n))
-end
+local name = "<HIER_DEN_PERIPHERAL_NAME>"
+local m = peripheral.getMethods(name)
+table.sort(m)
+for _,v in ipairs(m) do print(v) end
