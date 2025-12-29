ca63aa4889bfcde82250f198a6544a9768594225
B3nniB <benjaminbank99@gmail.com>
2025-12-29 06:59:39 +0100

    Turbine optik

--- CC_Reaktor/reaktor.lua
+++ CC_Reaktor/reaktor.lua
@@ -331,20 +331,20 @@ local function drawTurbineLive()
   writePad(monL, valX, y,     status, valW)
 
   -- Steam / Heated Coolant % (bei Turbine ist es Steam)
-  writePad(monL, valX+4, y+2,   steamPct and pct(steamPct) or "N/A", valW)
+  writePad(monL, valX+4, y+2,   steamPct and pct(steamPct) or "N/A", valW-4)
 
   -- Water %: nicht per turbineValve API vorhanden -> zeigen wir N/A
-  writePad(monL, valX+4, y+3,   "N/A", valW)
+  writePad(monL, valX+4, y+3,   "N/A", valW-4)
 
   -- Energy %
-  writePad(monL, valX+4, y+4,   energyPct and pct(energyPct) or "N/A", valW)
+  writePad(monL, valX+4, y+4,   energyPct and pct(energyPct) or "N/A", valW-4)
 
   -- Max Production / Production
   writePad(monL, valX, y+6,   maxProd and (string.format("%.0f FE/t", maxProd)) or "N/A", valW)
-  writePad(monL, valX+6, y+7,   prod    and (string.format("%.0f FE/t", prod))    or "N/A", valW)
+  writePad(monL, valX+6, y+7,   prod    and (string.format("%.0f FE/t", prod))    or "N/A", valW-6)
 
   -- Steam input
-  writePad(monL, valX+6, y+8,   steamIn and (string.format("%.0f mB/t", steamIn)) or "N/A", valW)
+  writePad(monL, valX+6, y+8,   steamIn and (string.format("%.0f mB/t", steamIn)) or "N/A", valW-6)
 
   -- Water Output: in deiner Liste gibt’s nur MAX, kein aktuelles Out -> zeigen wir max
   writePad(monL, valX, y+9,   maxWaterOut and (string.format("%.0f max", maxWaterOut)) or "N/A", valW)
