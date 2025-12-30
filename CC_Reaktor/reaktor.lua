a489335551b5e5abf28af7963ad68822458055c7
B3nniB <benjaminbank99@gmail.com>
2025-12-30 05:56:19 +0100

    ENDLICH Reaktor Screen V1.0

--- CC_Reaktor/reaktor.lua
+++ CC_Reaktor/reaktor.lua
@@ -457,7 +457,7 @@ local function drawMatrixLive()
 
   writePad(monL, valX+4, y,     fmtFE(cap, false),    valW-4)
   writePad(monL, valX+5, y+1,   fmtFE(stored, false), valW-5)
-  writePad(monL, valX+5, y+2,   (storedPct and string.format("%6.2f%%, storedPct")))
+  writePad(monL, valX+5, y+2,   (storedPct and string.format("%6.2f%%", storedPct) or "N/A"), valW-5)
   writePad(monL, valX+5, y+2,   fmtFE(input, true),   valW-5)
   writePad(monL, valX+5, y+3,   fmtFE(output, true),  valW-5)
   writePad(monL, valX+5, y+4,   fmtFE(change, true),  valW-5)
